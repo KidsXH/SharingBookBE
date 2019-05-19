7846384df346e8acd2b9de1bdda9413ad153aeb4
KidsXH <wlzeign@hotmail.com>
2019-05-20 00:07:33 +0800

    delete useless comments

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from users.views import UserLoginView, UserLogoutView, UserRegisterView, UserProfileView
 
 
-# The API URLs are now determined automatically by the router.
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='user-login'),
     path('logout/', UserLogoutView.as_view(), name='user-logout'),
